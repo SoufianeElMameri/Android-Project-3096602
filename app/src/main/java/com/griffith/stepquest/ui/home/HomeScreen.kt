9678b17dd4f1dc755cd4356d07f758a4ef218887
SoufianeElMameri <aztikireland@gmail.com>
2025-11-05 14:46:25 +0000

    add step progress circle

--- app/src/main/java/com/griffith/stepquest/ui/home/HomeScreen.kt
+++ app/src/main/java/com/griffith/stepquest/ui/home/HomeScreen.kt
@@ -1,5 +1,7 @@
 package com.griffith.stepquest.ui.home
 
+import android.health.connect.datatypes.StepsCadenceRecord
+import android.widget.GridLayout
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
@@ -13,6 +15,7 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.AccountCircle
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.ui.Alignment
@@ -33,8 +36,14 @@ fun HomeScreen() {
             .fillMaxSize()
             .background(bg)
 
-    ) {
-        HeaderBar()
+    ){
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally // centers circle horizontally
+        ) {
+            HeaderBar()
+            StepProgressCircle()
+        }
     }
 }
 
@@ -65,4 +74,32 @@ fun HeaderBar() {
             Text("30", fontWeight = FontWeight.SemiBold)
         }
     }
+}
+
+// header bar that holds the user and the amount of coins earned
+@Composable
+fun StepProgressCircle() {
+    // add space
+    Spacer(modifier = Modifier.height(40.dp))
+
+    Box(
+        modifier = Modifier
+            .size(240.dp),
+        contentAlignment = Alignment.Center
+    ){
+        CircularProgressIndicator(
+            progress = { 1f },
+            strokeWidth = 18.dp,
+            color = Color(0xFFE0E0E0),
+            modifier = Modifier.fillMaxSize()
+        )
+        CircularProgressIndicator(
+            progress = { 0.6F },
+            strokeWidth = 18.dp,
+            color = Color(0xFF4ADE80),
+            modifier = Modifier.fillMaxSize()
+        )
+
+    }
+
 }
