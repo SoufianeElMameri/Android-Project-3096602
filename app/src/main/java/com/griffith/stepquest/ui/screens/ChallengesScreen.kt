7f164cdc0d6747f8a09b47f5e5b8a087279f05b2
SoufianeElMameri <aztikireland@gmail.com>
2025-11-05 20:28:44 +0000

    create challenge card

--- app/src/main/java/com/griffith/stepquest/ui/screens/ChallengesScreen.kt
+++ app/src/main/java/com/griffith/stepquest/ui/screens/ChallengesScreen.kt
@@ -1,10 +1,15 @@
 package com.griffith.stepquest.ui.screens
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Star
 import androidx.compose.material.icons.rounded.ArrowBack
 import androidx.compose.material.icons.rounded.AccountCircle
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -32,6 +37,20 @@ fun ChallengesScreen() {
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
             ChallengesHeader()
+            Text(
+                text = "Today's Challenges",
+                fontSize = 18.sp,
+                fontWeight = FontWeight.Bold,
+                color = Color.DarkGray,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(start = 20.dp, top = 12.dp)
+            )
+            ChallengeCard(
+                title = "Walk 5,000 steps today",
+                progressText = "3,511 / 5,000",
+                reward = 5
+            )
         }
     }
 }
@@ -67,5 +86,68 @@ private fun ChallengesHeader() {
             tint = Color.DarkGray,
             modifier = Modifier.size(28.dp)
         )
+    }
+}
+
+
+@Composable
+fun ChallengeCard(title: String,progressText: String, reward: Int, modifier: Modifier = Modifier
+) {
+    Card(
+        modifier = modifier
+            .height(80.dp)
+            .fillMaxWidth()
+            .padding(horizontal = 4.dp),
+        shape = RoundedCornerShape(10.dp),
+        elevation = CardDefaults.cardElevation(defaultElevation = 4.dp),
+        colors = CardDefaults.cardColors(containerColor = Color.White)
+    ) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .fillMaxHeight()
+                .padding(horizontal = 12.dp),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.SpaceBetween
+        ){
+            Row(){
+                Icon(
+                    imageVector = Icons.Default.Star,
+                    contentDescription = "Challenge",
+                    tint = Color(0xFFFFC107),
+                    modifier = Modifier.size(32.dp)
+                )
+                Spacer(Modifier.width(20.dp))
+                Column {
+                    Text(
+                        text = title,
+                        fontSize = 14.sp,
+                        color = Color.DarkGray,
+                        fontWeight = FontWeight.Medium,
+                    )
+                    Text(
+                        text = progressText,
+                        color = Color.DarkGray,
+                        fontSize = 13.sp
+                    )
+                }
+            }
+            Box(
+                modifier = Modifier
+                    .background(
+                        color = Color(0xFF4ADE80),
+                        shape = RoundedCornerShape(12.dp)
+                    )
+                    .padding(horizontal = 12.dp, vertical = 6.dp)
+            ) {
+                Text(
+                    text = "+$reward",
+                    color = Color.White,
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 14.sp
+                )
+            }
+        }
+
     }
 }
